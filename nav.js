600cb5a41b7f24a6dcf7ffc68537c1291f3fadae
MrShyDiver <mr.shydiver@gmail.com>
2026-09-10 18:44:37 +0200

    Update nav.js

--- nav.js
+++ nav.js
@@ -16,7 +16,7 @@
         { fichier: "annonce-saison.html", label: "Le concept" },
         { fichier: "stats.html",          label: "Profil",     profil: true,        donnees: "players.json" },
         { fichier: "inventaire.html",     label: "Inventaire", profil: true,        donnees: "players.json" },
-        { fichier: "scout.html",          label: "Scout",     profil: true,        donnees: "players.json" },
+        { fichier: "scout.html",          label: "Scouter",     profil: true,        donnees: "players.json" },
         { fichier: "shop.html",           label: "Boutique",   profil: true, rechercheNav: false, donnees: "players.json" },
         { fichier: "ranking.html",        label: "Classements",                     donnees: "players.json" },
         { fichier: "codex.html",          label: "Codex",                           donnees: "catalogue_stats.json" }
